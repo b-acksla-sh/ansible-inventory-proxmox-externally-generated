7675bc19c948051c9963ef7d04677610cdfe5dca
rsa <code@b.acksla.sh>
2023-07-08 00:12:32 +0530

    fix: inventory created in same directory as script

--- ansible-inventory_proxmox.sh
+++ ansible-inventory_proxmox.sh
@@ -21,7 +21,7 @@
 #
 #
 nodes="$(ssh root@$PVE "cat /etc/pve/.members | jq -r '.nodelist[] | select(.online == 1) | .ip'")"
-echo "[proxmox_nodes]\n$nodes" > ansible-inventory_proxmox.ini
+echo "[proxmox_nodes]\n$nodes" > $(dirname "$0")/ansible-inventory_proxmox.ini
 #
 #
 #		EXTENDING:
